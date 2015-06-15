3e7cd22175ffe76e9e7adc81bc4147aec27e2fdb
joeajasin <Sholays52000@yahoo.com>
2015-06-15 05:15:56 -0400

    Update CustomerTest.java

--- src/test/java/com/abc/CustomerTest.java
+++ src/test/java/com/abc/CustomerTest.java
@@ -1,57 +1,73 @@
 package com.abc;
-
-import org.junit.Ignore;
-import org.junit.Test;
-
+    
+import org.junit.*;
 import static org.junit.Assert.assertEquals;
 
 public class CustomerTest {
-
-    @Test //Test customer statement generation
+	
+	@Test //Test customer statement generation
     public void testApp(){
 
-        Account checkingAccount = new Account(Account.CHECKING);
-        Account savingsAccount = new Account(Account.SAVINGS);
+        Account checkingAccount = new CheckingAccount(0.0, Constant.CHECKING);
+        Account savingsAccount = new SavingsAccount(0.0, Constant.SAVINGS);
+		Account maxiSavingsAccount = new MaxiSavingsAccount(0.0, Constant.MAXI_SAVINGS);
 
-        Customer henry = new Customer("Henry").openAccount(checkingAccount).openAccount(savingsAccount);
+        Customer henry = new Customer("Henry").openAccount(checkingAccount).openAccount(savingsAccount).openAccount(maxiSavingsAccount);
 
         checkingAccount.deposit(100.0);
+		
         savingsAccount.deposit(4000.0);
         savingsAccount.withdraw(200.0);
+		
+		maxiSavingsAccount.deposit(2000.0);
+        maxiSavingsAccount.withdraw(200.0);
 
-        assertEquals("Statement for Henry\n" +
-                "\n" +
-                "Checking Account\n" +
-                "  deposit $100.00\n" +
-                "Total $100.00\n" +
-                "\n" +
-                "Savings Account\n" +
-                "  deposit $4,000.00\n" +
-                "  withdrawal $200.00\n" +
-                "Total $3,800.00\n" +
-                "\n" +
-                "Total In All Accounts $3,900.00", henry.getStatement());
-    }
+        checkingAccount.transfer(savingsAccount, checkingAccount, 1000.0);
+        checkingAccount.transfer(maxiSavingsAccount, checkingAccount, 1000.0);
+
+        assertEquals("Statement for Henry\n" +           
+					"\n" +					   
+					"Checking Account\n" +
+					"  deposit $100.00\n" +
+					"  deposit $1,000.00\n" +
+					"  deposit $1,000.00\n" +
+					"Total $2,100.00\n" +
+					  "\n" +
+					"Savings Account\n" +
+					"  deposit $4,000.00\n" +
+					"  withdrawal ($200.00)\n" +
+					"  withdrawal ($1,000.00)\n" +
+					"Total $2,800.00\n" +
+					  "\n" +
+					"Maxi Savings Account\n" +
+					"  deposit $2,000.00\n" +
+					"  withdrawal ($200.00)\n" +
+					"  withdrawal ($1,000.00)\n" +
+					"Total $800.00\n" +
+					  "\n" +
+					"Total In All Accounts $5,700.00\n" +
+					"Number Of Accounts for Henry: 3\n" +
+					"Total Interest Earned by Henry: $8.30",  henry.getStatement());
+	}
 
     @Test
     public void testOneAccount(){
-        Customer oscar = new Customer("Oscar").openAccount(new Account(Account.SAVINGS));
+        Customer oscar = new Customer("Oscar").openAccount(new SavingsAccount(0.0, Constant.SAVINGS));
         assertEquals(1, oscar.getNumberOfAccounts());
     }
 
     @Test
     public void testTwoAccount(){
-        Customer oscar = new Customer("Oscar")
-                .openAccount(new Account(Account.SAVINGS));
-        oscar.openAccount(new Account(Account.CHECKING));
+        Customer oscar = new Customer("Oscar").openAccount(new SavingsAccount(0.0, Constant.SAVINGS));
+        oscar.openAccount(new CheckingAccount(0.0, Constant.CHECKING));
         assertEquals(2, oscar.getNumberOfAccounts());
     }
 
-    @Ignore
+    @Test
     public void testThreeAcounts() {
-        Customer oscar = new Customer("Oscar")
-                .openAccount(new Account(Account.SAVINGS));
-        oscar.openAccount(new Account(Account.CHECKING));
+        Customer oscar = new Customer("Oscar").openAccount(new SavingsAccount(0.0, Constant.SAVINGS));
+        oscar.openAccount(new CheckingAccount(0.0, Constant.CHECKING));
+		oscar.openAccount(new MaxiSavingsAccount(0.0, Constant.MAXI_SAVINGS));
         assertEquals(3, oscar.getNumberOfAccounts());
     }
 }
