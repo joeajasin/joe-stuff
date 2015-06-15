09e65ad759cf5b4d4f74ed477d00e80e35c6fbce
joeajasin <Sholays52000@yahoo.com>
2015-06-15 05:09:46 -0400

    Create Constant.java

---
+++ src/main/java/com/abc/Constant.java
@@ -0,0 +1,15 @@
+package com.abc; 
+
+public class Constant {
+
+	// Account types
+	public static final int CHECKING = 0;
+	public static final int SAVINGS = 1;
+	public static final int MAXI_SAVINGS = 2;
+
+	// Rate types
+  public static final double TEN_PERCENT_RATE = 0.1;
+	public static final double FIVE_PERCENT_RATE = 0.05;
+	public static final double TWO_PERCENT_RATE = 0.002;
+	public static final double ONE_PERCENT_RATE = 0.001;
+}
