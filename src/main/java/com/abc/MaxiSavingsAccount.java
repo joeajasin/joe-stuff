ebe9302f7b475224eb6fd2ca78cc162d7049c397
joeajasin <Sholays52000@yahoo.com>
2015-06-15 05:13:19 -0400

    Create MaxiSavingsAccount.java

---
+++ src/main/java/com/abc/MaxiSavingsAccount.java
@@ -0,0 +1,8 @@
+package com.abc;
+
+public class MaxiSavingsAccount extends Account {
+
+	public MaxiSavingsAccount(final double balance, final int accountType) {
+		super(balance, accountType);
+	}
+}
